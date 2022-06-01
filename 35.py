04b94eb3442679ca15c44a1ac137863acc4ebd08
allcabar88 <98633990+allcabar88@users.noreply.github.com>
2022-06-01 10:41:27 +0700

    Add files via upload

---
+++ 35.py
@@ -0,0 +1,15 @@
+# В файле находится N натуральных чисел, 
+# записанных через пробел. Среди чисел не 
+# хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найти его.
+
+data=open(r'E:\ucheba\python\lection 1\35\filenum.txt','r' ) 
+a=data.readline().split()
+
+for i in range(len(a)):
+    a[i]=int(a[i])
+for j in range(len(a)):
+    if a[j] - 1 == a[j - 1]:
+        continue
+    else:
+        print(a[j]-1)
+    
